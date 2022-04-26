889c4581b6b81de5d86cf9c41588a90ef95ac9c1
Jonathan Vazquez <yair.vazquez001@gmail.com>
2022-04-26 14:41:30 -0500

    boostraps

---
+++ javascript/tipos_datos/assets/js/tiposDatos.js
@@ -0,0 +1,38 @@
+/* Tipos de datos */
+
+/* let nombre = "Jonathan"; //String
+let edad = 23; //Number
+
+const nombreCompleto = "Jonathan Yair Vazquez";
+
+
+let tienesTrabajo = true; //Boleano
+let variableNull = null; //null
+
+let variableIndefinida;
+
+console.log(typeof(nombre))
+console.log(typeof(edad))
+console.log(typeof(tienesTrabajo))
+console.log(typeof(variableNull))
+console.log(typeof(variableIndefinida))
+ */
+
+
+
+
+/* Var, let y const */
+
+/* 
+
+if(true){
+    let numero = 10
+    console.log(numero)
+}
+
+for(let i=0; i<=10; i++){
+    console.log(i)
+}
+
+console.log(i) */
+
